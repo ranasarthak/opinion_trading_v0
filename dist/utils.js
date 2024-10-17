ec550f76221b315b5d18cca350de1bb252b9ea8d
sara6772 <sara.ran95@gmail.com>
2024-10-18 00:59:39 +0530

    cleanUp

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paiseToRupees = exports.rupeesToPaise = void 0;
-const rupeesToPaise = (rupees) => Math.round(rupees * 100);
-exports.rupeesToPaise = rupeesToPaise;
-const paiseToRupees = (paise) => paise / 100;
-exports.paiseToRupees = paiseToRupees;
--- dist/variables.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STOCK_BALANCES = exports.ORDERBOOK = exports.INR_BALANCES = void 0;
-exports.INR_BALANCES = {};
-exports.ORDERBOOK = {};
-exports.STOCK_BALANCES = {};
